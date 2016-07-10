1c78ce84c0d70bb0be81a2356fbc980c0646e8e8
John Grib <johngrib@woowahan.com>
2016-07-10 11:32:29 +0900

    ChromeDriver 설정 메소드 추가

--- src/config/Config.java
+++ src/config/Config.java
@@ -1,10 +1,11 @@
 package config;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import org.json.simple.JSONObject;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import lombok.Getter;
-import lombok.Setter;
 import util.FileHelper;
 import util.JSONHelper;
 
@@ -19,22 +20,41 @@ public class Config {
     private boolean async = false;
 
     @Getter
-    @Setter
     private JSONObject json;
 
-    public Config(String[] args) {
+    public Config(final String[] args) {
 
         new JCommander(this, args);
 
         final String location = new FileHelper().getAbsolutePath(source);
-        final JSONObject json = new JSONHelper().getJsonObject(location);
 
-        this.setJson(json);
+        this.json = new JSONHelper().getJsonObject(location);
+        this.build_options(this.json);
     }
 
-    public Config build_options(){
-
+    private Config build_options(JSONObject json) {
+        setChromeDriver(json);
         return this;
+    }
+
+    private void setChromeDriver(JSONObject json) {
+
+        final String chrome_drv = "chromedriver";
+        final String driver_key = "webdriver.chrome.driver";
+        final String error_text = "can't found chromedriver\n";
+
+        if (!json.containsKey(chrome_drv)) {
+            return;
+        }
+
+        final String chr_path = (String) json.get(chrome_drv);
+        final String abs_path = new FileHelper().getAbsolutePath(chr_path);
+
+        if (new File(abs_path).exists()) {
+            System.setProperty(driver_key, abs_path);
+        } else {
+            new FileNotFoundException(error_text);
+        }
     }
 
 }
