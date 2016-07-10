e6f9d4c7802545bdf97b52ac402c036993a48f7d
John Grib <johngrib@woowahan.com>
2016-07-10 21:42:26 +0900

    added command : close

---
+++ src/command/Close.java
@@ -0,0 +1,24 @@
+package command;
+
+import org.openqa.selenium.WebDriver;
+
+public class Close extends CommandProto {
+
+    @Override
+    public boolean is_enable() {
+        return this.driver != null;
+    }
+
+    @Override
+    public WebDriver execute() {
+        this.driver.quit();
+        this.driver = null;
+        return null;
+    }
+
+    @Override
+    public boolean is_success() {
+        return this.driver == null;
+    }
+
+}
