18f3b1b6c26dec9a4743f8b8c42c343749b39ae8
John Grib <johngrib@woowahan.com>
2016-07-10 21:41:32 +0900

    make program runnable

--- src/controller/Ruberdriver.java
+++ src/controller/Ruberdriver.java
@@ -1,16 +1,38 @@
 package controller;
 
+import org.junit.Test;
 import config.Config;
+import model.Item;
 
 public class Ruberdriver {
 
-    public Ruberdriver(Config cfg) {
+    public Ruberdriver() {
     }
 
     public static void main(String[] args) {
 
         Config cfg = new Config(args);
-        Ruberdriver rd = new Ruberdriver(cfg);
+        Ruberdriver rd = new Ruberdriver();
+        rd.run(cfg);
+        System.exit(0);
+    }
 
+    public void run(Config cfg) {
+
+        String s_name = cfg.getRunScenario();
+        Item scenario = cfg.getScenarios().get(s_name);
+
+        // https://groups.google.com/d/msg/webdriver/cw_awztl-IM/shC3BvJ0gVIJ
+        ScenarioExe exe = new ScenarioExe(scenario, cfg);
+        exe.execute();
+
+    }
+
+    @Test
+    public void mainTest() {
+        String[] args = { "--source", "test.json", "--scenario", "purchase_test" };
+        //String[] args = { "--source", "test.json", "--scenario", "login_test" };
+        //String[] args = { "--scenario", "go_test" };
+        Ruberdriver.main(args);
     }
 }
---
+++ src/controller/ScenarioExe.java
@@ -0,0 +1,69 @@
+package controller;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+import org.openqa.selenium.WebDriver;
+
+import command.Command;
+import config.Config;
+import model.Item;
+
+public class ScenarioExe implements Runnable {
+
+    private Item scenario;
+    private Config cfg;
+
+    public ScenarioExe(Item scenario, Config cfg) {
+        super();
+        this.scenario = scenario;
+        this.cfg = cfg;
+    }
+
+    @Override
+    public void run() {
+        execute();
+    }
+
+    public void execute() {
+
+        HashMap<String, Item> items = cfg.getItems();
+        LinkedList<String> scenes = scenario.getList();
+        WebDriver driver = null;
+
+        for (String s : scenes) {
+            Item item = items.get(s);
+            LinkedList<String> user_strings = item.getList();
+
+            for (String sentence : user_strings) {
+                driver = execute(sentence, driver, cfg);
+            }
+        }
+    }
+
+    private WebDriver execute(String sentence, WebDriver driver, Config cfg) {
+        System.out.println(sentence);
+
+        String function = getFunction(sentence);
+        String param = getParam(sentence);
+
+        Command command = cfg.getRegister().getCommand(function);
+
+        command.prepare(driver, cfg, param);
+
+        if (command.is_enable()) {
+            driver = command.execute();
+            command.is_success();
+        }
+        return driver;
+    }
+
+    private String getFunction(String sentence) {
+        return sentence.replaceFirst("\\s.*", "");
+    }
+
+    private String getParam(String sentence) {
+        return sentence.replaceFirst("^[A-Za-z]+\\s", "");
+    }
+
+}
