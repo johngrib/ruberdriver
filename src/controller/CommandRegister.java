7f4e3496b0a402c7e1f3f3acad54ce8dd993171e
John Grib <johngrib@woowahan.com>
2016-07-10 21:32:20 +0900

    added CommandRegister

---
+++ src/controller/CommandRegister.java
@@ -0,0 +1,52 @@
+package controller;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import command.Command;
+
+public class CommandRegister {
+
+    private HashMap<String, Command> commands;
+
+    public CommandRegister() {
+        super();
+        commands = new HashMap<>();
+    }
+
+    public Command getCommand(String function) {
+        if (commands.containsKey(function)) {
+            return commands.get(function);
+        }
+
+        try {
+
+            final String class_name = "command." + function;
+
+            @SuppressWarnings("unchecked")
+            Class<Command> cmd_class = (Class<Command>) Class.forName(class_name);
+            Constructor<Command> constructor = cmd_class.getConstructor();
+
+            Command cmd = constructor.newInstance(new Object[] {});
+            commands.put(function, cmd);
+
+            return cmd;
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    };
+}
