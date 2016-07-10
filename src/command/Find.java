177999a5369d23d5af8bd1af598b2155ba1860dc
John Grib <johngrib@woowahan.com>
2016-07-10 23:47:07 +0900

    added command : find

--- src/command/Find.java
+++ src/command/Find.java
@@ -1,12 +1,43 @@
 package command;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class Find extends CommandProto {
 
     @Override
     public WebDriver execute() {
-        return driver;
+
+        String function = getFunction(this.param);
+        String query = getParam(this.param);
+
+        try {
+
+            Method method = By.class.getMethod(function, String.class);
+            By by = (By) method.invoke(By.class, query);
+            
+            WebElement element = this.driver.findElement(by);
+
+            cfg.setLastElement(element);
+
+        } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException | SecurityException e) {
+            e.printStackTrace();
+        }
+
+        return this.driver;
     }
 
+    protected String getFunction(String sentence) {
+        return sentence.replaceFirst("\\s.*", "");
+    }
+
+    protected String getParam(String sentence) {
+        return sentence.replaceFirst("^[A-Za-z]+\\s", "");
+    }
 }
