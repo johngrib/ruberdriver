04d8ae0e28c293ef1ce79c71b79fe4710bfa179c
John Grib <johngrib@woowahan.com>
2016-07-12 21:17:33 +0900

    added comment syntax : # (comment), $ (break loop), !$ (force quit)

--- src/config/Const.java
+++ src/config/Const.java
@@ -4,4 +4,7 @@ public class Const {
 
     public static final String ITEM = "item";
     public static final String SCENARIO = "scenario";
+    public static final String COMMENT = "#";
+    public static final String COMMENT_QUIT = "$";
+    public static final String COMMENT_FORCE_QUIT = "!$";
 }
