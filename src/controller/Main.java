dead06cbdacb2807d29568a9e95cd416fe86e125
John Grib <johngrib@woowahan.com>
2016-07-16 22:54:22 +0900

    added --all option

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,28 +1,62 @@
 package controller;
 
+import java.util.Iterator;
+import java.util.LinkedList;
 import org.junit.Test;
-
 import config.Config;
+import config.Options;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Config cfg = new Config(args);
-        Ruberdriver rd = new Ruberdriver(cfg);
-        rd.start();
-        try {
-            rd.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        Main main = new Main();
+        Options option = new Options(args);
+        Config cfg = new Config(option);
+
+        if (option.isAsync()) {
+            main.runAsync(cfg, option);
+        } else {
+            new Ruberdriver(cfg).run();
+            System.exit(0);
         }
+    }
+
+    public void runAsync(Config cfg, Options option) {
+
+        if (option.isAllScenario()) {
+
+            LinkedList<Thread> threads = new LinkedList<>();
+
+            Iterator<String> it = cfg.getScenarios().keySet().iterator();
+
+            while (it.hasNext()) {
+                String scenario = (String) it.next();
+                Thread rd = new Ruberdriver(cfg, scenario);
+                threads.add(rd);
+                rd.start();
+            }
+
+            Iterator<Thread> tit = threads.iterator();
+            try {
+                while (tit.hasNext()) {
+                    Thread thread = (Thread) tit.next();
+                    thread.join();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return;
     }
 
     @Test
     public void mainTest() {
-        String[] args = { "--source", "test.json", "--scenario", "purchase_test", "-d" };
-        //String[] args = { "--source", "test.json", "--scenario", "login_test" };
-        //String[] args = { "--scenario", "go_test" };
+        // String[] args = { "--source", "test.json", "--scenario", "purchase_test" };
+        String[] args = { "--source", "test.json", "--scenario", "purchase_test", "-a", "-l" };
+        // String[] args = { "--source", "test.json", "--scenario", "purchase_test", "-d" };
+        // String[] args = { "--source", "test.json", "--scenario", "login_test" };
+        // String[] args = { "--scenario", "go_test" };
         Main.main(args);
     }
 }
--- src/controller/Ruberdriver.java
+++ src/controller/Ruberdriver.java
@@ -7,20 +7,34 @@ import scenariorunner.ScenarioRunner;
 public class Ruberdriver extends Thread {
 
     private Config cfg;
+    private String scenario_name;
 
     public Ruberdriver(Config cfg) {
         this.cfg = cfg;
     }
 
+    public Ruberdriver(Config cfg, String scenario_name) {
+        this.cfg = cfg;
+        this.scenario_name = scenario_name;
+    }
+
     public void run() {
 
-        String s_name = this.cfg.getOption().getRunScenario();
-        Item scenario = this.cfg.getScenarios().get(s_name);
+        Item scenario = this.cfg.getScenarios().get(getScenarioName());
 
         // https://groups.google.com/d/msg/webdriver/cw_awztl-IM/shC3BvJ0gVIJ
         ScenarioRunner exe = new ScenarioRunnerSelector(this.cfg).getScenarioRunner();
         exe.run(scenario);
 
+    }
+
+    private String getScenarioName() {
+
+        if (this.scenario_name != null) {
+            return this.scenario_name;
+        } else {
+            return this.cfg.getOption().getRunScenario();
+        }
     }
 
 }
