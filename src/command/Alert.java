e5a48a99eb6e184957f999075dc7691db6d803f4
John Grib <johngrib@woowahan.com>
2016-07-18 22:05:47 +0900

    added defined method stamp command : Alert

--- src/command/Alert.java
+++ src/command/Alert.java
@@ -1,9 +1,15 @@
 package command;
 
+import java.lang.reflect.Method;
+import java.util.Set;
+import java.util.TreeSet;
 import org.openqa.selenium.WebDriver;
+import checker.ParameterDefinedMethod;
 import checker.ParameterNotNull;
 
-public class Alert extends CommandProto implements ParameterNotNull {
+public class Alert extends CommandProto implements ParameterNotNull, ParameterDefinedMethod {
+
+    private Set<String> param_methods;
 
     @Override
     public boolean is_enable() {
@@ -14,16 +20,35 @@ public class Alert extends CommandProto implements ParameterNotNull {
     public WebDriver execute() {
 
         org.openqa.selenium.Alert alert = this.driver.switchTo().alert();
- 
-        if("accept".equals(this.param)){
+
+        if ("accept".equals(this.param)) {
             alert.accept();
         } else {
             alert.dismiss();
         }
-        
+
         this.driver.switchTo().defaultContent();
 
         return this.driver;
+    }
+
+    @Override
+    public Set<String> get_defined_param_methods() {
+        if (this.param_methods == null) {
+            this.param_methods = new TreeSet<>();
+
+            Method[] ms = org.openqa.selenium.Alert.class.getDeclaredMethods();
+
+            for (Method method : ms) {
+                Class<?>[] pType = method.getParameterTypes();
+                Class<?> rType = method.getReturnType();
+
+                if (pType.length == 0 && "void".equals(rType.getName())) {
+                    this.param_methods.add(method.getName());
+                }
+            }
+        }
+        return this.param_methods;
     }
 
 }
